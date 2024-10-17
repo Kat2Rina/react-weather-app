b87b92f26d78c0354de1851822ba74c239e7406a
Karina <777katrina@gmail.com>
2024-10-17 11:22:47 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Karina Kushnirenko and is{" "}
-        <a href="https://github.com/Kat2Rina/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Karina Kushnirenko and is{" "}
+          <a
+            href="https://github.com/Kat2Rina/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
