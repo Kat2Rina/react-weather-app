2f5b907e950669a57182b7336c670fe2f1a6078a
Karina <777katrina@gmail.com>
2024-10-24 12:19:55 +0200

    updated netlify url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
             open-sourced on GitHub
           </a>{" "}
           and hosted on{" "}
-          <a href="https://github.com/Kat2Rina/react-weather-app">Netlify</a>
+          <a href="https://magenta-yeot-308d19.netlify.app/">Netlify</a>
         </footer>
       </div>
     </div>
